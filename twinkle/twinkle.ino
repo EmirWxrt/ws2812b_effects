669e634c9b882a1dec0dc3f69f8687fd2aafeac4
srepla <alpers.christopher@outlook.de>
2019-11-17 18:35:28 +0100

    add twinkle

---
+++ twinkle/twinkle.ino
@@ -0,0 +1,27 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN 6
+#define LED_CNT 144
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_CNT, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
+  randomSeed(42);
+}
+
+void loop() {
+  twinkle(strip.Color(0, 127, 40), 20, 100, 100);
+}
+
+void twinkle(uint32_t color, int twinkleCnt, int twinkleWait, int cycleWait) {
+
+  for (int i = 0; i < twinkleCnt; i++) {
+    strip.setPixelColor(random(0, LED_CNT), color);
+    strip.show();
+    delay(twinkleWait);
+  }
+  strip.clear();
+  strip.show();
+  delay(cycleWait);
+}
